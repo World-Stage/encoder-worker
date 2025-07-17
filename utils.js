163f2a65bfd33c6e3782a8405c43b62471c91581
John Fletcher <johnfletcherdev@gmail.com>
2025-07-16 19:43:25 -0600

    initial implementation

--- utils.js
+++ utils.js
@@ -1,4 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
 // Utility functions for the encoder worker
+function deleteDirectoryRecursive(dirPath) {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(dirPath)) {
+      console.log(`Directory does not exist: ${dirPath}`);
+      return resolve();
+    }
+    console.log(`Deleting directory: ${dirPath}`);
+    fs.rm(dirPath, { recursive: true, force: true }, (err) => {
+      if (err) {
+        console.error(`Error deleting directory ${dirPath}:`, err);
+        reject(err);
+      } else {
+        console.log(`Successfully deleted directory: ${dirPath}`);
+        resolve();
+      }
+    });
+  });
+}
+
 module.exports = {
-  // Add your utility functions here
+  deleteDirectoryRecursive,
 };
